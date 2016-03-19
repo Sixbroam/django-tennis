3c878a4f0cf97d14f8a8e21df3fde5ec32184711
ambroise <ambrosix@gmail.com>
2016-03-19 21:57:49 +0100

    * Created api with DRF

--- django_tennis/settings/base.py
+++ django_tennis/settings/base.py
@@ -59,8 +59,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_tennis',
-        'USER': 'postgres',
-        'PASSWORD': 'kree',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
